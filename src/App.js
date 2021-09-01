7c4dec652178e7783ccae1b53ee6394a31b52435
taylortreece <tayloratreece@gmail.com>
2021-08-31 19:50:48 -0500

    commit tree 4e924193e80020f9ea85dc6086639eb036305d63

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
-// create your App component here
+import React from 'react' 
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            people: [],
+            peopleNames: []
+        };
+    }
+
+    componentDidMount() {
+        let url = "http://api.open-notify.org/astros.json"
+
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                people: data.people
+            })
+        })
+    }
+
+    getNames = () => {
+        if(this.state.people !== []) {
+            return this.state.people.map(person => {return person.name})
+        } else {
+            "ain't no one in space, esse"
+        }
+    }
+
+    render() {
+        return (
+            <h1>{this.getNames() + " " + "\n"}</h1>
+        )
+    }
+}
+
+export default App;
